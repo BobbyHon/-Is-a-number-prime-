a9c3fe11e84b269e0a51047a6b06f8b1d2604c19
BobbyHon <bryanthuynh35@yahoo.com>
2024-06-12 11:29:06 -0700

    5 Kyu

---
+++ main.js
@@ -0,0 +1,11 @@
+function isPrime(num){
+    if (!(num > 1)) return false
+    
+    for (let i = 2; i <= Math.pow(num, 1/2); i++){
+      if ((num / i) % 1 == 0)
+        {
+          return false
+        }
+    }
+    return true
+  }
